b4037bdb57eb639cee6ad99e8f8266cc2fee7cae
vinodkumarDV <dvvinod143@gmail.com>
2022-02-16 20:22:45 +0530

    created a expo app

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Login from './src/components/Login'; //completed
+import Singup from './src/components/Singup'; //completed
+import Resetpassword from './src/components/Resetpassword'; //completed
+import MyProfile from './src/components/MyProfile'; //completed
+import Profile from './src/components/Profile'; //completed
+import EditProfile from './src/components/EditProfile';
+import Connections from './src/components/Connections';
+import List from './src/components/List';
+import Messages from './src/components/Messages';
+import Textmgs from './src/components/Textmgs';
+import Post from './src/components/Post';
+import Setings from './src/components/Setings'
+
+function App() {
+  return (
+    <View>
+      <Login />
+    </View>
+  )
+};
+
+export default App;
+
+
